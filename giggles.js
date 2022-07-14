dbb870a6811cc96919cb7813cc1ab601fad9d0e9
Chuckle <55614189+qxchuckle@users.noreply.github.com>
2022-07-14 15:18:27 +0800

    Update giggles.js

--- giggles.js
+++ giggles.js
@@ -5,7 +5,7 @@ self.addEventListener('fetch', async event => {
 const handle = async (req) => {
     const domain = req.url.split('/')[2];
     if (domain.match("unpkg.com")) {
-        return fetch(req.url.replace("https://transfer.chuckle.top/", "https://www.chuckle.top/"));
+        return fetch(req.url.replace("https://transfer.chuckle.top", "https://www.chuckle.top"));
     }
     else {
         return fetch(req)
