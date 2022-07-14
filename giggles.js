6503110350eccb123b1a24c5c8372578b6e8ef45
Chuckle <55614189+qxchuckle@users.noreply.github.com>
2022-07-14 16:16:08 +0800

    Update giggles.js

--- giggles.js
+++ giggles.js
@@ -1,6 +1,18 @@
+const CACHE_NAME = 'ICDNCache';
+let cachelist = [];
+
+let cachelist = [
+];
+
 self.addEventListener('install', async function (installEvent) {
     self.skipWaiting();
-    installEvent.waitUntil();
+    installEvent.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(cachelist);
+            })
+    );
 });
 
 self.addEventListener('fetch', async event => {
@@ -8,13 +20,5 @@ self.addEventListener('fetch', async event => {
 });
 
 const handle = async (req) => {
-    const res = await fetch(req)
-    const resp = res.clone()
-    if (!!resp.headers.get('content-type')) {
-            return new Response((await resp.text()).replace(/transfer/g, 'www'), {
-                headers: resp.headers,
-                status: resp.status
-            })
-    }
-    return resp
+    return fetch(req.url.replace("https://transfer.chuckle.top/", "https://www.chuckle.top/"));
 }
