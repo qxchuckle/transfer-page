d390a7c0aa22c60b8ab1c1a11a1d685c6b60c29f
Chuckle <55614189+qxchuckle@users.noreply.github.com>
2022-07-14 16:23:00 +0800

    Update giggles.js

--- giggles.js
+++ giggles.js
@@ -20,7 +20,7 @@ const handle = async (req) => {
     const res = await fetch(req)
     const resp = res.clone()
     if (!!resp.headers.get('content-type')) {
-            return new Response((await resp.text()).replace(/transfer/g, 'www'), {
+            return new Response((await resp.text()).replace('transfer', 'www'), {
                 headers: resp.headers,
                 status: resp.status
             })
