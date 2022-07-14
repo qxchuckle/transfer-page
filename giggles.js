9886d00427ad29fcb71083561ae4c77186400990
Chuckle <55614189+qxchuckle@users.noreply.github.com>
2022-07-14 15:15:24 +0800

    Add files via upload

---
+++ giggles.js
@@ -0,0 +1,13 @@
+self.addEventListener('fetch', async event => {
+    event.respondWith(handle(event.request))
+});
+
+const handle = async (req) => {
+    const domain = req.url.split('/')[2];
+    if (domain.match("unpkg.com")) {
+        return fetch(req.url.replace("https://transfer.chuckle.top/", "https://www.chuckle.top/"));
+    }
+    else {
+        return fetch(req)
+    }
+}
