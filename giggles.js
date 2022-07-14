38ca28475ee40a6f002ecc6dcdf5a5e4d4d7c4a6
Chuckle <55614189+qxchuckle@users.noreply.github.com>
2022-07-14 15:23:18 +0800

    Update giggles.js

--- giggles.js
+++ giggles.js
@@ -1,13 +1,24 @@
+const CACHE_NAME = 'ICDNCache';
+let cachelist = [];
+
+let cachelist = [
+];
+
+self.addEventListener('install', async function (installEvent) {
+    self.skipWaiting();
+    installEvent.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(cachelist);
+            })
+    );
+});
+
 self.addEventListener('fetch', async event => {
     event.respondWith(handle(event.request))
 });
 
 const handle = async (req) => {
-    const domain = req.url.split('/')[2];
-    if (domain.match("unpkg.com")) {
-        return fetch(req.url.replace("https://transfer.chuckle.top", "https://www.chuckle.top"));
-    }
-    else {
-        return fetch(req)
-    }
+    return fetch(req.url.replace("https://transfer.chuckle.top/", "https://www.chuckle.top/"));
 }
